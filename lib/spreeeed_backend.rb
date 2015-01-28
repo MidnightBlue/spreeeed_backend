57a891e4255bb95c42e79e19828bd5e06b1c90bd
Bruce Sung <isfore@gmail.com>
2015-01-28 16:12:28 +0800

    test css

--- lib/spreeeed_backend.rb
+++ lib/spreeeed_backend.rb
@@ -1,9 +1,12 @@
 require "spreeeed_backend/engine"
 
 module SpreeeedBackend
-  initializer :assets do |config|
-    Rails.application.config.assets.precompile += %w{ spreeeed_backend.js }
-    Rails.application.config.assets.precompile += %w{ spreeeed_backend.css }
-    Rails.application.config.assets.paths << root.join("app", "assets")
+  class Engine < Rails::Engine
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w{ spreeeed_backend.js }
+      Rails.application.config.assets.precompile += %w{ spreeeed_backend.css }
+      Rails.application.config.assets.paths << root.join("app", "assets")
+    end
+
   end
 end
