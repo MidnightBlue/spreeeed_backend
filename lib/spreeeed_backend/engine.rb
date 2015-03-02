3bdc188c62f5c15f840adc4fc3a9468d361782d8
Bruce Sung <isfore@gmail.com>
2015-03-02 18:18:00 +0800

    Test

--- lib/spreeeed_backend/engine.rb
+++ lib/spreeeed_backend/engine.rb
@@ -1,10 +1,12 @@
 module SpreeeedBackend
   require 'devise'
+  require 'spreeeed_backend/active_record_extend'
 
   class Engine < ::Rails::Engine
     isolate_namespace SpreeeedBackend
 
-    config.autoload_paths << File.expand_path("../lib/spreeeed_backend/active_record_extension", __FILE__)
+    # config.autoload_paths << File.expand_path("../lib/spreeeed_backend/active_record_extension", __FILE__)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
   end
 end
