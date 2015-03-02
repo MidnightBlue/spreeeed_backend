dd4d21c51ba995ff885218af7c3f0e0dfcc81aac
Bruce Sung <isfore@gmail.com>
2015-03-02 23:08:35 +0800

    menu helper

--- app/helpers/spreeeed_backend/application_helper.rb
+++ app/helpers/spreeeed_backend/application_helper.rb
@@ -1,10 +1,48 @@
 module SpreeeedBackend
   module ApplicationHelper
+    # include Backend::CgRenderInputHelper
+    # include Backend::DisplayAttributeHelper
+    include SpreeeedBackend::MenuHelper
+
 
     def parent_layout(layout)
       # http://m.onkey.org/nested-layouts-in-rails-3
       @view_flow.set(:layout, output_buffer)
       self.output_buffer = render(:file => "layouts/#{layout}")
+    end
+
+    def search_box_placeholder(klass, searchable_cols)
+      res = t('search') + searchable_cols.collect do |col|
+        klass.human_attribute_name(col.to_sym)
+      end.join(', ')
+    end
+
+
+    def edit_label
+      "<i class='fa fa-pencil'></i> #{t('edit')}".html_safe
+    end
+
+    def frontend_view_label
+      "<i class='fa fa-eye'></i> #{t('frontend_view')}".html_safe
+    end
+
+    def destroy_label
+      "<i class='fa fa-trash-o'></i> #{t('destroy')}".html_safe
+    end
+
+    def attachment_type(attachment)
+      css = case attachment.content_type
+              when 'image/jpeg'
+                "fa-file-image-o"
+              when 'application/pdf'
+                "fa-file-pdf-o"
+              when 'application/msword'
+                "fa-file-word-o"
+              else
+                "fa-file-o"
+            end
+
+      %Q|<i class="fa #{css}"></i>|.html_safe
     end
 
   end
---
+++ app/helpers/spreeeed_backend/menu_helper.rb
@@ -0,0 +1,64 @@
+module SpreeeedBackend
+  module MenuHelper
+
+    def render_role_menu
+      menu = ''
+
+      items =
+          if current_user.is_root?
+            root_menu
+          end
+
+      items.each do |label, attrs|
+        if attrs[:path]
+          menu += render_menu(label, attrs)
+        else
+          sub_menu   = ''
+          paths      = []
+          attrs.each do |_label, _attrs|
+            paths << _attrs[:path].split('/')[1]
+            sub_menu += render_menu(_label, _attrs)
+          end
+          open       = paths.include?(params[:controller].to_s)
+          open_css   = (open ? ' open' : '')
+          block_css  = (open ? ' style="display: block;"' : '')
+
+          menu += %Q|<li class="parent#{open_css}"><a href="#"><i class="fa fa-folder-open"></i><span>#{label}</span></a>|
+          menu += %Q|<ul class="sub-menu"#{block_css}>|
+          menu += sub_menu
+          menu += "</ul>"
+          menu += "</li>"
+        end
+      end
+
+      menu.html_safe
+
+    end
+
+    def root_menu
+      menu = ActiveSupport::OrderedHash.new
+      menu[t('frontend').to_sym] = {
+          :icon_css => 'fa-home',
+          :path     => root_path,
+      }
+
+      menu[User.model_name.human.to_sym] = {
+          :icon_css => User.icon,
+          :path     => send("#{SpreeeedBackend.name_space}_users_path"),
+      }
+
+      menu
+    end
+
+    def render_menu(label, attrs)
+      icon_css   = attrs[:icon_css]
+      path       = attrs[:path]
+
+      active     = (path == root_path ? (request.path == path) : request.path.match(path))
+      active_css = (active ? ' class="active"' : '')
+
+      %Q|<li#{active_css}><a href="#{path}"><i class="fa #{icon_css}"></i><span>#{label}</span></a></li>|
+    end
+
+  end
+end
