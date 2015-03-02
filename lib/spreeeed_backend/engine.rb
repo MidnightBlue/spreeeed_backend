7202311036d5a1fb1d334094cfc1d27057a35ccc
Bruce Sung <isfore@gmail.com>
2015-03-02 16:58:32 +0800

    add extension

--- lib/spreeeed_backend/engine.rb
+++ lib/spreeeed_backend/engine.rb
@@ -1,10 +1,9 @@
 module SpreeeedBackend
   require 'devise'
-  require 'active_record_extension'
 
   class Engine < ::Rails::Engine
     isolate_namespace SpreeeedBackend
 
-    config.autoload_paths << File.expand_path("../../lib", __FILE__)
+    require 'active_record_extension'
   end
 end
