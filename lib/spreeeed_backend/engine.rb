b8a911513a4c3b2f132748b1cc7ee016ee96b4c4
Bruce Sung <isfore@gmail.com>
2015-03-02 22:43:43 +0800

    add paginate

--- lib/spreeeed_backend/engine.rb
+++ lib/spreeeed_backend/engine.rb
@@ -3,11 +3,7 @@ module SpreeeedBackend
   require 'spreeeed_backend/active_record_extend'
 
   class Engine < ::Rails::Engine
-    # config.autoload_paths << File.expand_path("../lib/spreeeed_backend/active_record_extension", __FILE__)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    puts config.root
-
-
 
     isolate_namespace SpreeeedBackend
   end
--- spreeeed_backend.gemspec
+++ spreeeed_backend.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 3.2.21"
   s.add_dependency "jquery-rails"
   s.add_dependency "devise", "3.0.3"
+  s.add_dependency "bootstrap-will_paginate", "0.0.10"
 
 
   # s.add_dependency "rails-assets-bootstrap"
