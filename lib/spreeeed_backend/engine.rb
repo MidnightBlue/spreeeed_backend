1fdd78151972f8083a38fc83ceb703be4293830d
Bruce Sung <isfore@gmail.com>
2015-03-02 22:55:05 +0800

    add config

--- lib/spreeeed_backend/engine.rb
+++ lib/spreeeed_backend/engine.rb
@@ -4,6 +4,15 @@ module SpreeeedBackend
 
   require 'spreeeed_backend/active_record_extend'
 
+  class << self
+    mattr_accessor :name_space
+    self.name_space = 'backend'
+  end
+
+  def self.setup(&block)
+    yield self
+  end
+
   class Engine < ::Rails::Engine
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
