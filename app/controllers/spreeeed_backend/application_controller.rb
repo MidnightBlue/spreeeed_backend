6d143398e73c3300ad586bb90bd3c86468b04b49
Bruce Sung <isfore@gmail.com>
2015-03-02 16:33:16 +0800

    test

--- app/controllers/spreeeed_backend/application_controller.rb
+++ app/controllers/spreeeed_backend/application_controller.rb
@@ -8,6 +8,15 @@ module SpreeeedBackend
 
     before_filter :authenticate_user!, :setup_global_variables
 
+    def setup_global_variables
+      @klass            ||= NilClass
+      @klass_name       = @klass.name
+      @displayable_cols = (@klass_name == 'NilClass' ? [] : @klass.displayable_cols)
+      @editable_cols    = (@klass_name == 'NilClass' ? [] : @klass.editable_cols)
+      @nested_cols      = (@klass_name == 'NilClass' ? [] : @klass.nested_cols)
+      @hidden_cols      = (@klass_name == 'NilClass' ? [] : @klass.hidden_cols)
+    end
+
     def index
     end
 
