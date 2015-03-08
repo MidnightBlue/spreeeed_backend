a5e414a621911bd24dd29c48f1f15710590cb33b
Bruce Sung <isfore@gmail.com>
2015-03-09 00:31:44 +0800

    js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,4 +4,21 @@
 //= require jquery
 //= require jquery_ujs
 //= require spreeeed_backend/bootstrap/dist/js/bootstrap.js
-
+//= require cocoon
+//= require spreeeed_backend/jasny.bootstrap/extend/js/jasny-bootstrap.js
+//= require spreeeed_backend/jquery.gritter/js/jquery.gritter.js
+//= require spreeeed_backend/jquery.parsley/parsley.js
+//= require spreeeed_backend/jquery.parsley/i18n/messages.zh_tw
+//= require spreeeed_backend/jquery.ui/jquery-ui
+//= require spreeeed_backend/jquery.datatables/jquery.datatables.min
+//= require spreeeed_backend/jquery.datatables/bootstrap-adapter/js/datatables
+//= require spreeeed_backend/jquery.icheck/icheck.js
+//= require spreeeed_backend/bootstrap.datetimepicker/js/bootstrap-datetimepicker.js
+//= require spreeeed_backend/bootstrap.datetimepicker/js/locales/bootstrap-datetimepicker.zh-TW
+//= require spreeeed_backend/bootstrap.daterangepicker/moment.js
+//= require spreeeed_backend/bootstrap.daterangepicker/daterangepicker
+//= require spreeeed_backend/bootstrap.switch/bootstrap-switch.min
+//= require spreeeed_backend/jquery.niftymodals/js/jquery.modalEffects
+//= require spreeeed_backend/fuelux/loader.js
+//= require spreeeed_backend/jquery.nanoscroller/jquery.nanoscroller
+//= require spreeeed_backend/jquery.select2/select2.js
