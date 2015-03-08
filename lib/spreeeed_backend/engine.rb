99aa03310996aabe9d1eca9bccc10e1f8fb6b34b
Bruce Sung <isfore@gmail.com>
2015-03-09 00:40:02 +0800

    images

--- lib/spreeeed_backend/engine.rb
+++ lib/spreeeed_backend/engine.rb
@@ -16,15 +16,10 @@ module SpreeeedBackend
   end
 
   class Engine < ::Rails::Engine
-    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    # config.autoload_paths += Dir["#{config.root}/config/locales/**/"]
-
-    # initializer "my_engine.configure_i18n_initialization" do |config|
-      config.time_zone = 'Taipei'
-      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
-      config.i18n.available_locales ||= [:'zh-TW']
-      config.i18n.default_locale = :'zh-TW'
-    # end
+    config.time_zone = 'Taipei'
+    config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    config.i18n.available_locales ||= [:'zh-TW']
+    config.i18n.default_locale = :'zh-TW'
 
     isolate_namespace SpreeeedBackend
   end
