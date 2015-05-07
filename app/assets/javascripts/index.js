54ce7d8069b407439b015ce858677815774750e5
Bruce Sung <isfore@gmail.com>
2015-05-07 16:04:45 +0800

    update chart js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -26,3 +26,9 @@
 //= require spreeeed_backend/behaviour/core.js
 //= require spreeeed_backend/jquery.select2/select2.js
 // require spreeeed_backend/jquery.select2/select2_locale_zh-TW.js
+
+//= require spreeeed_backend/jquery.easypiechart/jquery.easy-pie-chart.js
+//= require spreeeed_backend/jquery.flot/jquery.flot.js
+//= require spreeeed_backend/jquery.flot/jquery.flot.pie.js
+//= require spreeeed_backend/jquery.flot/jquery.flot.resize.js
+//= require spreeeed_backend/jquery.flot/jquery.flot.labels.js
