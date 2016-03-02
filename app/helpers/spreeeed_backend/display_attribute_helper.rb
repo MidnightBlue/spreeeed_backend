03fe7b7a09355dc647745339a3c5267458cae1e2
Steven Tseng <booda406@gmail.com>
2016-03-02 17:55:57 +0800

    fix display attribute

--- app/helpers/spreeeed_backend/display_attribute_helper.rb
+++ app/helpers/spreeeed_backend/display_attribute_helper.rb
@@ -4,9 +4,7 @@ module SpreeeedBackend
     def display_attribute(object, attr)
       if association = detect_association(object, attr)
         association_object = object.send(association.to_sym)
-        if association_object.present? and association_object.respond_to?(:name)
-          return association_object.name
-        end
+        return association_object.name if association_object.present? and association_object.respond_to?(:name)
       end
 
       if attr.to_s == 'aasm_state'
